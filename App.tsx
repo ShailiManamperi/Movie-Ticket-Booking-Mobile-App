52d2f73d6d78782670aa26e2ad8250804d508c94
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-07-22 12:02:44 +0530

    update files

--- App.tsx
+++ App.tsx
@@ -1,108 +1,32 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+/* eslint-disable */
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import styles from "./src/componets/styles/Styles.tsx";
-import CustomIcons from "./src/componets/CustomIcons.tsx";
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import HomeScreen from "./src/Screens/HomeScreen.tsx";
 import { upcomingMovies,popularMovies,nowPlayingMovies,searchMovies} from "./src/Api/apiCalls.tsx";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
+//
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-
-  console.log(searchMovies('Avengers'));
-
+  const movie = searchMovies('Uranus2324');
+  console.log(movie);
+  console.log(searchMovies('Uranus2324'));
+  // @ts-ignore
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-          {/*<CustomIcons name={'search'} size={25}/>*/}
-          {/*<CustomIcons name={'ticket'} size={25}/>*/}
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step Two">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <HomeScreen/>
+
+    </View>
   );
-}
-
-
+};
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 40,
+    paddingHorizontal: 16,
+  },
+});
--- src/Api/apiCalls.tsx
+++ src/Api/apiCalls.tsx
@@ -1,21 +1,16 @@
-const apiKey: string = '753049c9feaee5416b7f1b3fe918f733';
-
-// Correct URLs for different endpoints
-export const nowPlayingMovies: string = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
-export const upcomingMovies: string = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`;
-export const popularMovies: string = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;  // Fixed typo
-
-export const searchMovies = (keyword: string) => {
-    return `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${keyword}`;  // Fixed the URL structure
-};
-
-export const movieDetails = (id: number) => {
-    return `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`;
-};
-
-export const movieCreditsDetails = (id: number) => {
-    return `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}`;
-};
+const apikey: string = '3301c7e7e8b8a0359d1274da575e20d1';
 export const baseImagePath = (size: string, path: string) => {
     return `https://image.tmdb.org/t/p/${size}${path}`;
+};
+export const nowPlayingMovies: string = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}`;
+export const upcomingMovies: string = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apikey}`;
+export const popularMovies: string = `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}`;
+export const searchMovies = (keyword: string) => {
+    return `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${keyword}`;
+};
+export const movieDetails = (id: number) => {
+    return `https://api.themoviedb.org/3/movie/${id}?api_key=${apikey}`;
+};
+export const movieCastDetails = (id: number) => {
+    return `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apikey}`;
 };
