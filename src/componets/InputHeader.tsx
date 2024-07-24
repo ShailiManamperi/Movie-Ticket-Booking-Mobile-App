7d632fbadd447688b5cb7e5fd77aafb4961132aa
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-07-24 12:21:19 +0530

    add some components

---
+++ src/componets/InputHeader.tsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {
+  BORDERRADIUS,
+  COLOR,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../Themes/themes.tsx';
+import CustomIcon from './CustomIcon';
+
+const InputHeader = (props: any) => {
+  const [searchText, setSearchText] = useState<string>('');
+  return (
+    <View style={styles.inputBox}>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={textInput => setSearchText(textInput)}
+        value={searchText}
+        placeholder="Search your Movies..."
+        placeholderTextColor={COLOR.WhiteRGBA32}
+      />
+      <TouchableOpacity
+        style={styles.searchIcon}
+        onPress={() => props.searchFunction(searchText)}>
+        <CustomIcon
+          name="search"
+          color={COLOR.Orange}
+          size={FONTSIZE.size_20}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputBox: {
+    display: 'flex',
+    paddingVertical: SPACING.space_8,
+    paddingHorizontal: SPACING.space_24,
+    borderWidth: 2,
+    borderColor: COLOR.WhiteRGBA15,
+    borderRadius: BORDERRADIUS.radius_25,
+    flexDirection: 'row',
+  },
+  textInput: {
+    width: '90%',
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLOR.White,
+  },
+  searchIcon: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: SPACING.space_10,
+  },
+});
+
+export default InputHeader;
