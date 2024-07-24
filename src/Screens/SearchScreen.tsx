df5e7ba5dd56cca86793b77d8f31716fff7112b5
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-07-24 15:37:19 +0530

    update SearchScreen (almost finish)

--- src/Screens/SearchScreen.tsx
+++ src/Screens/SearchScreen.tsx
@@ -1,23 +1,85 @@
-/* eslint-disable */
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  StatusBar,
+  FlatList,
+} from 'react-native';
+import {COLOR, SPACING} from "../Themes/themes.tsx";
+import {baseImagePath, searchMovies} from "../Api/apiCalls.tsx";
+import InputHeader from "../componets/InputHeader.tsx";
+import SubMovieCard from "../componets/SubMovieCard.tsx";
 
-const SearchScreen = () => {
+const {width, height} = Dimensions.get('screen');
+
+const SearchScreen = ({navigation}: any) => {
+  const [searchList, setSearchList] = useState([]);
+
+  const searchMoviesFunction = async (name: string) => {
+    try {
+     // console.log(name);
+      let response = await fetch(searchMovies(name));
+      let json = await response.json();
+      setSearchList(json.results);
+    } catch (error) {
+      console.error('Something went wrong in searchMoviesFunction ', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>SearchScreen</Text>
+      <StatusBar hidden />
+
+      <View>
+        <FlatList
+          data={searchList}
+          keyExtractor={(item: any) => item.id}
+          bounces={false}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          ListHeaderComponent={
+            <View style={styles.InputHeaderContainer}>
+              <InputHeader searchFunction={searchMoviesFunction} />
+            </View>
+          }
+          contentContainerStyle={styles.centerContainer}
+          renderItem={({item, index}) => (
+            <SubMovieCard
+              shoudlMarginatedAtEnd={false}
+              shouldMarginatedAround={true}
+              cardFunction={() => {
+                navigation.push('MovieDetails', {movieid: item.id});
+              }}
+              cardWidth={width / 2 - SPACING.space_12 * 2}
+              title={item.original_title}
+              imagePath={baseImagePath('w342', item.poster_path)}
+            />
+          )}
+        />
+      </View>
     </View>
   );
 };
 
-
-
 const styles = StyleSheet.create({
-  container: {},
-  text:{
-    color:'black',
-    fontSize:16,
-  }
+  container: {
+    display: 'flex',
+    flex: 1,
+    width,
+    alignItems: 'center',
+    backgroundColor: COLOR.Black,
+  },
+  InputHeaderContainer: {
+    display: 'flex',
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_28,
+    marginBottom: SPACING.space_28 - SPACING.space_12,
+  },
+  centerContainer: {
+    alignItems: 'center',
+  },
 });
 
 export default SearchScreen;
